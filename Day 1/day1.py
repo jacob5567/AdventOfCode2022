5effbd4246e7816c700971272ea66b4aa404cb90
Jacob Faulk <jacob5567@gmail.com>
2022-11-30 22:34:15 -0800

    day 1 part 2

--- Day 1/day1.py
+++ Day 1/day1.py
@@ -3,6 +3,7 @@ def main():
     input_list = input_file.readlines()
     input_file.close()
     print(find_max_calories(input_list))
+    print(find_top3_sum(input_list))
 
 def find_max_calories(input_list):
     current_max = 0
@@ -15,6 +16,25 @@ def find_max_calories(input_list):
             if current_sum > current_max:
                 current_max = current_sum
     return current_max
+
+def find_top3_sum(input_list):
+    sums = list()
+    current_sum = 0
+    num_elves = 0
+    for line in input_list:
+        if line == "\n":
+            num_elves += 1
+            if sums == []:
+                sums = [current_sum]
+            else:
+                i = 0
+                while(i != len(sums) and current_sum > sums[i]):
+                    i += 1
+                sums[i:i] = [current_sum]
+            current_sum = 0
+        else:
+            current_sum += int(line)
+    return sum(sums[-3:])
 
 if __name__ == "__main__":
     main()
