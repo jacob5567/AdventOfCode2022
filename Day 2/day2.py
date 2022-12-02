9cc521597d38e4f0b4c241767cf8495dd051f4ef
Jacob Faulk <jacob5567@gmail.com>
2022-12-02 00:24:24 -0800

    add day 2 input

---
+++ Day 2/day2.py
@@ -0,0 +1,10 @@
+def main():
+    with open("input", "r") as input_file:
+        input_list = input_file.readlines()
+
+def calculate_score(input_list):
+    pass
+
+
+if __name__ == "__main__":
+    main()
