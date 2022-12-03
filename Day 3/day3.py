e4943177ec256a573d884d19330d307c669cde3d
Jacob Faulk <jacob5567@gmail.com>
2022-12-02 21:03:36 -0800

    day 3 input

---
+++ Day 3/day3.py
@@ -0,0 +1,10 @@
+def main():
+    with open("input", "r") as input_file:
+        input_list = input_file.readlines()
+    print(sum_priorities(input_list))
+
+def sum_priorities(input_list):
+    pass
+
+if __name__ == "__main__":
+    main()
