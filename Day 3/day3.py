2cbcae18c0ce5c76cec2f8d034a16269fdcf5cbb
Jacob Faulk <jacob5567@gmail.com>
2022-12-02 21:20:41 -0800

    day 3 part 1

--- Day 3/day3.py
+++ Day 3/day3.py
@@ -3,8 +3,30 @@ def main():
         input_list = input_file.readlines()
     print(sum_priorities(input_list))
 
+
 def sum_priorities(input_list):
-    pass
+    priority_map = get_priority_map()
+    sum = 0
+    for line in input_list:
+        first_half = line[:len(line) // 2]
+        second_half = line[len(line) // 2:]
+        repeats = set(first_half).intersection(set(second_half))
+        for n in repeats:
+            sum += priority_map[n]
+    return sum
+
+
+def get_priority_map():
+    priority_map = {}
+    i = 1
+    for letter in (chr(n) for n in range(ord("a"), ord("z") + 1)):
+        priority_map[letter] = i
+        i += 1
+    for letter in (chr(n) for n in range(ord("A"), ord("Z") + 1)):
+        priority_map[letter] = i
+        i += 1
+    return priority_map
+
 
 if __name__ == "__main__":
     main()
