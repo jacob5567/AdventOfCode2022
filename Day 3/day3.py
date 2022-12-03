34ec88360654b712f4a5dc276104e9e9ba64114a
Jacob Faulk <jacob5567@gmail.com>
2022-12-02 21:27:04 -0800

    day 3 part 2

--- Day 3/day3.py
+++ Day 3/day3.py
@@ -2,6 +2,7 @@ def main():
     with open("input", "r") as input_file:
         input_list = input_file.readlines()
     print(sum_priorities(input_list))
+    print(sum_group(input_list))
 
 
 def sum_priorities(input_list):
@@ -15,6 +16,20 @@ def sum_priorities(input_list):
             sum += priority_map[n]
     return sum
 
+def sum_group(input_list):
+    stripped_list = [line.strip() for line in input_list]
+    priority_map = get_priority_map()
+    sum = 0
+    i = 0
+    while i < len(stripped_list) - 2:
+        bag_1 = stripped_list[i]
+        bag_2 = stripped_list[i + 1]
+        bag_3 = stripped_list[i + 2]
+        repeats = set(bag_1).intersection(set(bag_2)).intersection(set(bag_3))
+        for n in repeats:
+            sum += priority_map[n]
+        i += 3
+    return sum
 
 def get_priority_map():
     priority_map = {}
