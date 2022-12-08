9b71fdfcfcd572549674dc015156069393b98312
Jacob Faulk <jacob5567@gmail.com>
2022-12-07 21:46:44 -0800

    day 8 part 2

--- Day 8/day8.py
+++ Day 8/day8.py
@@ -1,8 +1,9 @@
 def main():
     with open("input", "r") as input_file:
         input_list = input_file.readlines()
-        tree_grid = [[int(t) for t in l.strip()] for l in input_list]
+    tree_grid = [[int(t) for t in l.strip()] for l in input_list]
     print(count_visible_trees(tree_grid))
+    print(find_highest_scenic_score(tree_grid))
 
 
 def count_visible_trees(tree_grid):
@@ -13,6 +14,16 @@ def count_visible_trees(tree_grid):
                 if check_visible(tree_grid, r, c):
                     num_visible += 1
     return num_visible
+
+
+def find_highest_scenic_score(tree_grid):
+    highest_score = 0
+    for r in range(len(tree_grid)):
+        for c in range(len(tree_grid[0])):
+            current_score = scenic_score(tree_grid, r, c)
+            if current_score > highest_score:
+                highest_score = current_score
+    return highest_score
 
 
 def check_visible(tree_grid, r, c):
@@ -39,6 +50,35 @@ def check_visible(tree_grid, r, c):
             sides_invisible += 1
             break
     return sides_invisible != 4
+
+
+def scenic_score(tree_grid, r, c):
+    height = tree_grid[r][c]
+    # check left
+    left_score = 0
+    for i in reversed(tree_grid[r][:c]):
+        left_score += 1
+        if i >= height:
+            break
+    # check right
+    right_score = 0
+    for i in tree_grid[r][c+1:]:
+        right_score += 1
+        if i >= height:
+            break
+    # check up
+    up_score = 0
+    for i in reversed(range(r)):
+        up_score += 1
+        if tree_grid[i][c] >= height:
+            break
+    # check down
+    down_score = 0
+    for i in range(r+1, len(tree_grid)):
+        down_score += 1
+        if tree_grid[i][c] >= height:
+            break
+    return left_score * right_score * up_score * down_score
 
 
 if __name__ == "__main__":
