6131bce51f91824dc4c2410c5e6c9046107f49a4
Jacob Faulk <jacob5567@gmail.com>
2022-12-10 13:25:15 -0800

    day 10 part 2

--- Day 10/day10.py
+++ Day 10/day10.py
@@ -2,6 +2,7 @@ def main():
     with open("input", "r") as input_file:
         input_list = input_file.readlines()
     print(calculate_signal_strength(input_list))
+    draw_screen(input_list)
 
 
 watched_cycles = [x * 40 + 20 for x in range(6)]
@@ -25,11 +26,42 @@ def calculate_signal_strength(input_list):
     return strength_sum
 
 
+def draw_screen(input_list):
+    cycle_number = 0
+    current_x = 1
+    cycle_map = {}
+
+    for line in input_list:
+        if line.strip() == "noop":
+            cycle_number, current_x, cycle_map = increment_and_store(
+                cycle_number, current_x, cycle_map)
+        elif line.split(" ")[0] == "addx":
+            cycle_number, current_x, cycle_map = increment_and_store(
+                cycle_number, current_x, cycle_map)
+            cycle_number, current_x, cycle_map = increment_and_store(
+                cycle_number, current_x, cycle_map)
+            current_x += int(line.split(" ")[1])
+
+    for i in range(1, 241):
+        if i % 40 - 1 in range(cycle_map[i]-1, cycle_map[i]+2):
+            print("#", end="")
+        else:
+            print(".", end="")
+        if i % 40 == 0:
+            print("")
+
+
 def increment_and_check(cycle_number, current_x, strength_sum):
     cycle_number += 1
     if cycle_number in watched_cycles:
         strength_sum += (current_x * cycle_number)
     return cycle_number, current_x, strength_sum
+
+
+def increment_and_store(cycle_number, current_x, cycle_map):
+    cycle_number += 1
+    cycle_map[cycle_number] = current_x
+    return cycle_number, current_x, cycle_map
 
 
 if __name__ == "__main__":
