5b2d61f87b70072b65ab40163a800124db53fc77
Jacob Faulk <jacob5567@gmail.com>
2022-12-11 15:54:49 -0800

    day 11 part 1

--- Day 11/day11.py
+++ Day 11/day11.py
@@ -7,6 +7,10 @@ class Monkey:
         self.test_false = test_false
         self.items = starting_items
 
+    def do_operation(self, item):
+        old = item
+        return eval(self.operation)
+
     def __str__(self):
         return (
             f"Monkey {self.number}:\n"
@@ -20,11 +24,15 @@ class Monkey:
     def __repr__(self):
         return self.__str__()
 
+    def print_items(self):
+        print(f"Monkey {self.number}: {', '.join(map(str, self.items))}")
+
 
 def main():
     with open("input", "r") as input_file:
         input = input_file.read()
-    read_monkeys(input)
+    monkey_list = read_monkeys(input)
+    print(monkey_business(monkey_list, 20))
 
 
 def read_monkeys(input):
@@ -33,15 +41,36 @@ def read_monkeys(input):
     for block in input.split("\n\n"):
         split_data = block.split("\n")
         current_info[0] = int(split_data[0].split(" ")[1][:-1])
-        current_info[1] = split_data[2].split(":")[1].strip()
+        current_info[1] = split_data[2].split(
+            ":")[1].strip().split("=")[1].strip()
         current_info[2] = int(split_data[3].split(" ")[-1])
         current_info[3] = int(split_data[4].split(" ")[-1])
         current_info[4] = int(split_data[5].split(" ")[-1])
         current_info[5] = [int(item) for item in split_data[1].split(":")[
             1].strip().split(", ")]
         monkeys.append(Monkey(*current_info))
-    print(*monkeys, sep="\n\n")
     return monkeys
+
+
+def monkey_business(monkeys, num_rounds):
+    activity_map = {}
+    for i in range(num_rounds):
+        for monkey in monkeys:
+            for j in range(len(monkey.items)):
+                # inspect item
+                if monkey.number not in activity_map:
+                    activity_map[monkey.number] = 0
+                activity_map[monkey.number] += 1
+                item = monkey.items.pop(0)
+                # execture operation
+                item = monkey.do_operation(item)
+                # monkey gets bored
+                item = item // 3
+                # throw item
+                monkeys[monkey.test_true if item % monkey.test ==
+                        0 else monkey.test_false].items.append(item)
+    top_two = sorted(activity_map.values())[-2:]
+    return top_two[0] * top_two[1]
 
 
 if __name__ == "__main__":
