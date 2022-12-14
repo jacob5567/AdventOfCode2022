6bf7fb7102f0d5582f1e5b9e6260593772bdf7a8
Jacob Faulk <jacob5567@gmail.com>
2022-12-13 22:16:49 -0800

    day 14 part 2

--- Day 14/day14.py
+++ Day 14/day14.py
@@ -1,7 +1,9 @@
 def main():
     with open("input", "r") as input_file:
         input_list = input_file.readlines()
-    cave = generate_cave(input_list)
+    cave = generate_cave_bottomless(input_list)
+    print(simulate_sand(cave))
+    cave = generate_cave_with_floor(input_list)
     print(simulate_sand(cave))
 
 
@@ -9,7 +11,7 @@ def simulate_sand(cave):
     start_point = (0, 500)
     num_sand = 0
     sand = start_point
-    while sand[0] < len(cave) - 1:
+    while sand[0] < len(cave) - 1 and cave[start_point[0]][start_point[1]] != "o":
         sand = start_point
         while cave[sand[0]][sand[1]] != "o":
             if sand[0] == len(cave) - 1:
@@ -29,7 +31,7 @@ def simulate_sand(cave):
     return num_sand
 
 
-def generate_cave(input_list):
+def generate_cave_bottomless(input_list):
     max_length = 0
     max_depth = 0
     for line in input_list:
@@ -67,10 +69,54 @@ def generate_cave(input_list):
     return cave
 
 
+def generate_cave_with_floor(input_list):
+    ADDITIONAL_LENGTH = 500
+    max_length = 0
+    max_depth = 0
+    for line in input_list:
+        if line != "\n":
+            line = line.split(" -> ")
+            for pair in line:
+                x, y = pair.split(",")
+                if int(x) > max_length:
+                    max_length = int(x)
+                if int(y) > max_depth:
+                    max_depth = int(y)
+    cave = [["." for _ in range(max_length + ADDITIONAL_LENGTH)]
+            for _ in range(max_depth + 3)]
+
+    # add start point
+    cave[0][500] = "+"
+
+    # add rocks
+    for line in input_list:
+        if line != "\n":
+            line = line.split(" -> ")
+            prev_x, prev_y = -1, -1
+            for pair in line:
+                y, x = pair.split(",")
+                x, y = int(x), int(y)
+                cave[x][y] = "#"
+                if prev_x != -1 and prev_y != -1:
+                    if prev_x == x:
+                        for i in range(min(prev_y, y), max(prev_y, y) + 1):
+                            cave[x][i] = "#"
+                    elif prev_y == y:
+                        for i in range(min(prev_x, x), max(prev_x, x) + 1):
+                            cave[i][y] = "#"
+                prev_x, prev_y = x, y
+
+    # add floor
+    for i in range(len(cave[0])):
+        cave[-1][i] = "#"
+
+    return cave
+
+
 def print_cave(cave):
     for i in range(len(cave)):
         print(i, end="\t")
-        for j in range(490, len(cave[0])):
+        for j in range(450, len(cave[0])):
             print(cave[i][j], end="")
         print()
 
